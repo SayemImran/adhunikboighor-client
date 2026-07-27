c69f25ca251ce69bd505f61c9b2b109d7b17328b
SayemImran <ik262142@gmail.com>
2026-07-27 12:37:40 +0600

    Pin better-auth to exact version

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
-import { jwtClient } from "better-auth/client/plugins";
+import { inferAdditionalFields, jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { auth } from "./auth";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL,
-    plugins:[jwtClient()],
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+    plugins: [inferAdditionalFields<typeof auth>(), jwtClient()],
 });
